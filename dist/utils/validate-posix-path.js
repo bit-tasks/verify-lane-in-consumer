577e947f81af04e147b54dd6b53049d4f91cc979
Eden <eden@bit.dev>
2024-09-11 15:13:15 +0300

    build action

--- dist/utils/validate-posix-path.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isValidPOSIXPath = void 0;
-function isValidPOSIXPath(path) {
-    const posixPathPattern = /^(\/|\.\/|\.\.\/)?([^\/\0]+(\/)?)+$/;
-    return posixPathPattern.test(path);
-}
-exports.isValidPOSIXPath = isValidPOSIXPath;
